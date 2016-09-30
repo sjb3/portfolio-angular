6d530b8ead801b770b40e4ac6ec9ee6854cb3850
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2016-09-29 17:06:56 -0700

    gallery?

--- app/entry.js
+++ app/entry.js
@@ -15,8 +15,6 @@ angular.module('appPortfolio', [ngRoute, ngDialog])
   $routeProvider
   .when('/', {
     template: require('./view/home/home.html'),
-    // controller: 'HomeController',
-    // controllerAs: 'homeCtrl',
   })
   .when('/main', {
     template: require('./view/main/main.html'),
@@ -38,4 +36,7 @@ require('./view/home');
 require('./view/ngDialog');
 require('./view/main');
 require('./view/about');
+require('./view/emailNgDialog');
+require('./view/phoneNgDialog');
+// require('./view/gallery');
 // require('./app-main');
---
+++ app/view/gallery/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//adapted from https://www.sitepoint.com/creating-slide-show-plugin-angularjs/
+
+const angular = require('angular');
+const appPortfolio = angular.module('appPortfolio');
