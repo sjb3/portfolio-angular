de8268ed807c6c2f8dada3c7f068770d137358f7
Sung J Byun <sungjbyun@D-69-91-157-96.dhcp4.washington.edu>
2016-11-27 19:02:43 -0800

    idk if i wanna keep gallery

--- app/view/main/index.js
+++ app/view/main/index.js
@@ -1,28 +1,3 @@
-// 'use strict';
-//
-// require('./main.scss');
-//
-// const angular = require('angular');
-// const appPortfolio = angular.module('appPortfolio');
-//
-// appPortfolio.controller('MainNavController', ['$log', '$location', MainNavController]);
-//
-// function MainNavController($log, $location){
-//   $log.debug('MainNavController');
-//
-//   this.homeFontAwesome = function(){
-//     $location.path('/');
-//   };
-//
-//   this.aboutFontAwesome = function(){
-//     $location.path('/about');
-//   };
-//
-//   // this.emailFontAwesome = function(){
-//   //   $location('www.github.com/sjb3');
-//   // };
-// }
-
 'use strict';
 
 require('./main.scss');
